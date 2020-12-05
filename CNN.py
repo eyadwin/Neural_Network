7c46b230cb3f4bc22b70f9879562027bc9c78f68
eyadwin <dr.iyad.aboudoush@gmail.com>
2020-12-05 15:07:51 +0300

    Convolution NN-adding configuration notes

--- CNN.py
+++ CNN.py
@@ -17,11 +17,12 @@ from conv import Conv3x3
 from maxpool import MaxPool2
 from softmax import Softmax
 
-# We only use the first 1k testing examples (out of 10k total)
-# in the interest of time. Feel free to change this if you want.
-
-test_images = mnist.MNIST('data/').test_images()[:1000]
-test_labels = mnist.MNIST('data/').test_labels()[:1000]
+# We only use the first 1k examples of each set in the interest of time.
+# Feel free to change this if you want.
+train_images = mnist.MNIST('data/').train_images[:1000]
+train_labels = mnist.MNIST('data/').train_labels[:1000]
+test_images = mnist.MNIST('data/').test_images[:1000]
+test_labels = mnist.MNIST('data/').test_labels[:1000]
 
 conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
 pool = MaxPool2()                  # 26x26x8 -> 13x13x8
@@ -46,21 +47,64 @@ def forward(image, label):
 
   return out, loss, acc
 
+def train(im, label, lr=.005):
+  '''
+  Completes a full training step on the given image and label.
+  Returns the cross-entropy loss and accuracy.
+  - image is a 2d numpy array
+  - label is a digit
+  - lr is the learning rate
+  '''
+  # Forward
+  out, loss, acc = forward(im, label)
+
+  # Calculate initial gradient
+  gradient = np.zeros(10)
+  gradient[label] = -1 / out[label]
+
+  # Backprop
+  gradient = softmax.backprop(gradient, lr)
+  gradient = pool.backprop(gradient)
+  gradient = conv.backprop(gradient, lr)
+
+  return loss, acc
+
 print('MNIST CNN initialized!')
 
+# Train the CNN for 3 epochs
+for epoch in range(3):
+  print('--- Epoch %d ---' % (epoch + 1))
+
+  # Shuffle the training data
+  permutation = np.random.permutation(len(train_images))
+  train_images = train_images[permutation]
+  train_labels = train_labels[permutation]
+
+  # Train!
+  loss = 0
+  num_correct = 0
+  for i, (im, label) in enumerate(zip(train_images, train_labels)):
+    if i % 100 == 99:
+      print(
+        '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
+        (i + 1, loss / 100, num_correct)
+      )
+      loss = 0
+      num_correct = 0
+
+    l, acc = train(im, label)
+    loss += l
+    num_correct += acc
+
+# Test the CNN
+print('\n--- Testing the CNN ---')
 loss = 0
 num_correct = 0
-for i, (im, label) in enumerate(zip(test_images, test_labels)):
-  # Do a forward pass.
+for im, label in zip(test_images, test_labels):
   _, l, acc = forward(im, label)
   loss += l
   num_correct += acc
 
-  # Print stats every 100 steps.
-  if i % 100 == 99:
-    print(
-      '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
-      (i + 1, loss / 100, num_correct)
-    )
-    loss = 0
-    num_correct = 0
+num_tests = len(test_images)
+print('Test Loss:', loss / num_tests)
+print('Test Accuracy:', num_correct / num_tests)
